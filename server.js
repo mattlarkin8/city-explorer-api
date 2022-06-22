9977994ef0a73ea19d341c43da9e3a7411919cbf
Matthew Larkin <matthewlarkin8@gmail.com>
2022-06-22 19:55:09 -0400

    Created weather endpoint to provide weather data for front-end requests. Created a Forecast class that grooms the weather data before it is sent to the client.

--- server.js
+++ server.js
@@ -1,23 +1,43 @@
 'use strict';
 
-const express=require('express');
 require('dotenv').config();
-let data=require('./data/weather.json');
+const express=require('express');
+const cors=require('cors');
+const data=require('./data/weather.json');
 
 const app=express();
+app.use(cors());
+
 const PORT=process.env.PORT||3002;
 
 app.get('/weather',(request,response)=>{
-  let cityName=request.query.city;
-  // let lat=request.query.lat;
-  // let lon=request.query.lon;
-  // let city=data.find(obj=>obj.lat===lat&&obj.lon===lon);
-  let city=data.find(obj=>obj.city_name===cityName);
-  response.send(`${city}`);
+  try {
+    let searchQuery = request.query.searchQuery;
+    // let lat=request.query.lat;
+    // let lon=request.query.lon;
+    // let city=data.find(obj=>obj.lat===lat&&obj.lon===lon);
+    let searchResult = data.find(obj => obj.city_name.toLowerCase() === searchQuery.toLowerCase());
+    let result = searchResult.data.map(dayObj => new Forecast(dayObj));
+    response.status(200).send(result);
+  }
+  catch (error) {
+    next(error);
+  }
 });
 
 app.get('*',(request,response)=>{
-  response.send('The route you entered does not exist.');
+  response.status(404).send('The route you entered does not exist.');
+});
+
+class Forecast{
+  constructor(weatherObject){
+    this.datetime=weatherObject.datetime;
+    this.description=weatherObject.weather.description;
+  }
+}
+
+app.use((error,request,response,next)=>{
+  response.status(500).send(error.message);
 });
 
 app.listen(PORT,()=>console.log(`Listening on port ${PORT}`));
