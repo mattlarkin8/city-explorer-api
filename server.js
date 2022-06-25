0c18659350252226dde09794b043b2e7fdd9982c
Matthew Larkin <matthewlarkin8@gmail.com>
2022-06-24 23:18:48 -0400

    Fixed initial errors with code. Modified to also work with movie endpoint.

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
 'use strict';
 
 require('dotenv').config();
-const express=require('express');
-const cors=require('cors');
+const express = require('express');
+const cors = require('cors');
 
-const getWeather=require('./modules/weather');
-const getMovies=require('./modules/movies');
+const weather = require('./modules/weather.js');
+const movies=require('./modules/movies');
 
-const app=express();
+const app = express();
 app.use(cors());
 
 const PORT=process.env.PORT||3002;
 
-app.get('/weather',getWeather);
+app.get('/weather', weatherHandler);
 
-app.get('/movies',getMovies);
+app.get('/movies',movieHandler);
 
-app.get('*',(request,response)=>{
-  response.status(404).send('The route you entered does not exist.');
-});
+function weatherHandler(request, response) {
+  const { lat, lon } = request.query;
+  weather(lat, lon)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(200).send('Sorry. Trouble getting weather!');
+    });
+}
 
-app.listen(PORT,()=>console.log(`Listening on port ${PORT}`));
+function movieHandler(request, response) {
+  const city = request.query.city;
+  movies(city)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(200).send('Sorry. Trouble getting movies!');
+    });
+}
+
+app.listen(PORT, () => console.log(`Server up on ${PORT}`));
