87839ae87ba42c1f7d70b65f83526551eddc85de
Matthew Larkin <matthewlarkin8@gmail.com>
2022-06-24 23:20:37 -0400

    Fixed initial errors in code. Made neccessary adjustments to get cache working for weather.

--- modules/weather.js
+++ modules/weather.js
@@ -1,27 +1,42 @@
 'use strict';
 
 const axios=require('axios');
+let cache = require('./cache.js');
 
+async function getWeather(latitude, longitude) {
+  let lat=latitude;
+  let lon=longitude;
+  const key = 'weather: ' + lat + ', ' + lon;
+  const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHERBIT_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=5`;
 
-async function getWeather(req,res){
-  try{
-    let lat=req.query.lat;
-    let lon=req.query.lon;
-    let url=`http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHERBIT_API_KEY}&units=I&days=3&lat=${lat}&lon=${lon}`;
-    let result=await axios.get(url);
-    let groomedData=result.data.data.map(dayObj=>new Forecast(dayObj));
-    res.status(200).send(groomedData);
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit');
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = await axios.get(url)
+      .then(response => parseWeather(response.data));
   }
-  catch(error){
-    res.status(500).send(`Error getting weather: ${error.status}. ${error.message}`);
+  return cache[key].data;
+}
+
+function parseWeather(weatherData) {
+  try {
+    const weatherSummaries = weatherData.data.map(day => {
+      return new Weather(day);
+    });
+    return Promise.resolve(weatherSummaries);
+  } catch (e) {
+    return Promise.reject(e);
   }
 }
 
-class Forecast{
-  constructor(weatherObject){
-    this.datetime=weatherObject.datetime;
-    this.description=weatherObject.weather.description;
+class Weather {
+  constructor(day) {
+    this.forecast = day.weather.description;
+    this.time = day.datetime;
   }
 }
 
-module.exports=getWeather;
+module.exports = getWeather;
